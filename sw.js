4608bfab3c9565dc1b4383799eee49c6fe7061fb
skytins3-png <skytins3@gmail.com>
2026-09-05 20:57:55 +0900

    Refresh PWA cache for audio player update

--- sw.js
+++ sw.js
@@ -1,12 +1,11 @@
-const CACHE="ftf-mobile-v4";
+const CACHE="ftf-mobile-v5";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("activate",e=>e.waitUntil(Promise.all([
- self.clients.claim(),
- caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
-])));
+self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener("activate",e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener("fetch",e=>{
- e.respondWith(fetch(e.request).then(resp=>{
-   const copy=resp.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return resp;
- }).catch(()=>caches.match(e.request).then(r=>r||caches.match("./index.html"))));
+ if(e.request.mode==="navigate"){
+  e.respondWith(fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put("./index.html",copy));return resp}).catch(()=>caches.match("./index.html")));
+  return;
+ }
+ e.respondWith(fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp}).catch(()=>caches.match(e.request)));
 });
