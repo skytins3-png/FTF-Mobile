b09cd42b826c24660128d3a1c26c302bc64d6b86
skytins3-png <skytins3@gmail.com>
2026-09-09 15:07:43 +0900

    Complete FTF-02 26-scene playback mapping

--- ftf-cinematic-assets-01.js
+++ ftf-cinematic-assets-01.js
@@ -3,7 +3,7 @@
   const BASES={1:'/FTF-Mobile/assets/comics/FTF-01/',2:'/FTF-Mobile/assets/comics/FTF-02/'};
   const EXTRA={
     1:{10:'11.svg',11:'12.svg',12:'13.svg',13:'14.svg',14:'15.svg',15:'16.svg',16:'17.svg',17:'18.svg',18:'19.svg',19:'20.svg',20:'21.svg',21:'22.svg',22:'23.svg',23:'24.svg',24:'25.svg',25:'26.svg'},
-    2:{10:'11.svg',11:'12.svg',12:'13.svg',13:'14.svg',14:'15.svg',15:'16.svg',16:'17.svg',17:'18.svg',18:'19.svg',19:'20.svg'}
+    2:{10:'11.svg',11:'12.svg',12:'13.svg',13:'14.svg',14:'15.svg',15:'16.svg',16:'17.svg',17:'18.svg',18:'19.svg',19:'20.svg',20:'21.svg',21:'22.svg',22:'23.svg',23:'24.svg',24:'25.svg',25:'26.svg'}
   };
   const previous=window.showComic;
   if(typeof previous!=='function')return;
